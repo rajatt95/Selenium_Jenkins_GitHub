40a746f56616cec5abeeac7b4331a2aa8a9ea45d
rajat verma <rajat.verma@ATMECS.CORP>
2020-12-29 17:13:36 +0530

    Initial commit

---
+++ src/test/java/testcases/LoginTest.java
@@ -0,0 +1,36 @@
+
+package testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+
+	@Test
+	public void testAlert() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+
+		// Implicit wait
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		System.out.println("Title: " + driver.getTitle());
+		System.out.println("URL: " + driver.getCurrentUrl());
+		System.out.println("Success");
+
+		Thread.sleep(2000);
+		driver.quit();
+	}
+}
